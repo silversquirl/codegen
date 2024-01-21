496ab38f55229f5504ff931792f2d311227e1b0d
Silver <silver@squirl.dev>
2024-01-21 16:12:43 +0000

    feat(codegen): implement arithmetic instructions

--- src/codegen/rv64.zig
+++ src/codegen/rv64.zig
@@ -18,40 +18,28 @@ pub fn compile(
     const regs = try regalloc.physicalAlloc(rv64.Register, rv64.mutable_regs, allocator, func, liveness, vregs);
     defer regs.deinit(allocator);
 
-    for (func.blocks.items) |blk| {
-        for (blk.slice(func.insns), blk.slice(vregs), blk.slice(regs), 0..) |ins, vreg, reg, insn_i| {
-            const live = liveness.single(func.insns, blk.start, @enumFromInt(insn_i));
-            std.debug.print("{} {} {}\n", .{
-                ins.fmtWithLiveness(live),
-                vreg,
-                reg,
-            });
-        }
-    }
-
     var comp = try Compiler.init(allocator, func, liveness, regs);
     defer comp.deinit();
 
     // Count required instructions and generate label data
-    var count: u32 = 0;
     for (0..func.blocks.count()) |blk_idx| {
         const blk_ref: ssa.Block.Ref = @enumFromInt(blk_idx);
-        comp.labels.getPtr(blk_ref).* = count;
-        count += try comp.block(false, blk_ref, count);
+        comp.labels.getPtr(blk_ref).* = comp.offset;
+        try comp.block(blk_ref);
     }
 
     // Allocate code buffer
-    comp.code = try code_allocator.alloc(u8, @as(usize, @sizeOf(u32)) * count);
-    errdefer code_allocator.free(comp.code);
+    comp.code = try code_allocator.alloc(u8, @as(usize, @sizeOf(u32)) * comp.offset);
+    errdefer code_allocator.free(comp.code.?);
 
     // Generate code
-    count = 0;
+    comp.offset = 0;
     for (0..func.blocks.count()) |blk_idx| {
         const blk_ref: ssa.Block.Ref = @enumFromInt(blk_idx);
-        count += try comp.block(true, blk_ref, count);
+        try comp.block(blk_ref);
     }
 
-    return comp.code;
+    return comp.code.?;
 }
 
 const Compiler = struct {
@@ -60,7 +48,9 @@ const Compiler = struct {
     liveness: ssa.liveness.Info,
     regs: ssa.InstructionStore(rv64.Register),
     labels: util.IndexedStore(ssa.Block.Ref, u32).Mutable = .{},
-    code: []u8 = undefined,
+
+    offset: u32 = 0,
+    code: ?[]u8 = null,
 
     fn init(allocator: std.mem.Allocator, func: ssa.Function, liveness: ssa.liveness.Info, regs: ssa.InstructionStore(rv64.Register)) !Compiler {
         var comp: Compiler = .{
@@ -79,12 +69,12 @@ const Compiler = struct {
         comp.labels.deinit(comp.allocator);
     }
 
-    fn block(comp: *Compiler, comptime do_emit: bool, blk_ref: ssa.Block.Ref, start_offset: usize) !u32 {
+    fn block(comp: *Compiler, blk_ref: ssa.Block.Ref) !void {
         const blk = comp.func.blocks.getPtr(blk_ref);
-        var count: u32 = 0;
         for (blk.slice(comp.func.insns), blk.slice(comp.func.types), @intFromEnum(blk.start)..) |insn, ty, insn_idx| {
-            const offset = start_offset + count;
             const insn_ref: ssa.Instruction.Ref = @enumFromInt(insn_idx);
+            const reg = comp.regs.get(blk.start, insn_ref);
+
             switch (insn) {
                 .param => {}, // TODO
                 .expect => {},
@@ -93,37 +83,26 @@ const Compiler = struct {
 
                 .i_const => |value| {
                     // TODO: immediates
-                    const reg = comp.regs.get(blk.start, insn_ref);
                     switch (ty.kind()) {
                         .unsigned_int => {
                             if (std.math.cast(u12, value)) |v12| {
-                                if (do_emit) {
-                                    try comp.emit(offset, .{ .addi = .{ reg, .x0, v12 } });
-                                }
-                                count += 1;
+                                try comp.emit(.{ .addi = .{ reg, .x0, v12 } });
                             } else if (std.math.cast(u31, value)) |v31| {
-                                if (do_emit) {
-                                    const top = v31 & 0x7ffff000;
-                                    const bottom: u12 = @truncate(v31);
-                                    try comp.emit(offset, .{ .lui = .{ reg, top } });
-                                    try comp.emit(offset + 1, .{ .addi = .{ reg, reg, bottom } });
-                                }
-                                count += 2;
+                                const top = v31 & 0x7ffff000;
+                                const bottom: u12 = @truncate(v31);
+                                try comp.emit(.{ .lui = .{ reg, top } });
+                                try comp.emit(.{ .addi = .{ reg, reg, bottom } });
                             } else if (std.math.cast(u32, value)) |v32| {
                                 const lsb = value & 1 != 0;
 
-                                if (do_emit) {
-                                    const top = v32 >> 1 & 0x7ffff000;
-                                    const bottom: u12 = @truncate(v32 >> 1);
-                                    try comp.emit(offset, .{ .lui = .{ reg, top } });
-                                    try comp.emit(offset + 1, .{ .addi = .{ reg, reg, bottom } });
-                                    try comp.emit(offset + 2, .{ .slli = .{ reg, reg, 1 } });
-                                    if (lsb) {
-                                        try comp.emit(offset + 3, .{ .addi = .{ reg, reg, 1 } });
-                                    }
+                                const top = v32 >> 1 & 0x7ffff000;
+                                const bottom: u12 = @truncate(v32 >> 1);
+                                try comp.emit(.{ .lui = .{ reg, top } });
+                                try comp.emit(.{ .addi = .{ reg, reg, bottom } });
+                                try comp.emit(.{ .slli = .{ reg, reg, 1 } });
+                                if (lsb) {
+                                    try comp.emit(.{ .addi = .{ reg, reg, 1 } });
                                 }
-
-                                count += if (lsb) 4 else 3;
                             } else {
                                 @panic("TODO");
                             }
@@ -133,30 +112,56 @@ const Compiler = struct {
                     }
                 },
 
-                .true => {},
-                .false => {},
+                .true => try comp.emit(.{ .addi = .{ reg, .x0, 1 } }),
+                .false => try comp.emit(.{ .addi = .{ reg, .x0, 0 } }),
 
-                .add => {},
-                .sub => {},
-                .mul => {},
-                .div => {},
+                .add => |i| {
+                    const lhs = comp.regs.get(blk.start, i.lhs);
+                    const rhs = comp.regs.get(blk.start, i.rhs);
+                    try comp.emit(.{ .add = .{ reg, lhs, rhs } });
+                },
+                .sub => |i| {
+                    const lhs = comp.regs.get(blk.start, i.lhs);
+                    const rhs = comp.regs.get(blk.start, i.rhs);
+                    try comp.emit(.{ .sub = .{ reg, lhs, rhs } });
+                },
+                .mul => |i| {
+                    const lhs = comp.regs.get(blk.start, i.lhs);
+                    const rhs = comp.regs.get(blk.start, i.rhs);
+                    try comp.emit(.{ .mul = .{ reg, lhs, rhs } });
+                },
+                .div => |i| {
+                    const lhs = comp.regs.get(blk.start, i.lhs);
+                    const rhs = comp.regs.get(blk.start, i.rhs);
+                    try comp.emit(.{ .div = .{ reg, lhs, rhs } });
+                },
 
                 .eq => {},
                 .ne => {},
-                .lt => {},
+                .lt => |i| {
+                    const lhs = comp.regs.get(blk.start, i.lhs);
+                    const rhs = comp.regs.get(blk.start, i.rhs);
+                    try comp.emit(.{ .slt = .{ reg, lhs, rhs } });
+                },
                 .le => {},
-                .gt => {},
+                .gt => |i| {
+                    const lhs = comp.regs.get(blk.start, i.lhs);
+                    const rhs = comp.regs.get(blk.start, i.rhs);
+                    try comp.emit(.{ .slt = .{ reg, rhs, lhs } });
+                },
                 .ge => {},
 
                 .call => {},
             }
         }
-        return count;
     }
 
-    fn emit(comp: *Compiler, offset: usize, insn: rv64.Instruction) !void {
-        const op = try rv64.assembleInstruction(insn);
-        std.mem.writeInt(u32, comp.code[@sizeOf(u32) * offset ..][0..@sizeOf(u32)], op, .little);
+    fn emit(comp: *Compiler, insn: rv64.Instruction) !void {
+        if (comp.code) |code| {
+            const op = try rv64.assembleInstruction(insn);
+            std.mem.writeInt(u32, code[@sizeOf(u32) * comp.offset ..][0..@sizeOf(u32)], op, .little);
+        }
+        comp.offset += 1;
     }
 };
 
@@ -185,20 +190,18 @@ test "basic arithmetic" {
     try testGen(func, &.{
         .{ .addi = .{ .x1, .x0, 13 } },
         .{ .addi = .{ .x2, .x0, 7 } },
-        // .{ .add = .{ ..., .x3, .x4 } },
+        .{ .add = .{ .x1, .x1, .x2 } },
         .{ .addi = .{ .x2, .x0, 4 } },
-        // .{ .sub = .{ ..., ..., .x5 } },
+        .{ .sub = .{ .x1, .x1, .x2 } },
         .{ .addi = .{ .x3, .x0, 3 } },
-        // .{ .mul = .{ ..., ..., .x6 } },
-        // .{ .div = .{ ..., ..., .x5 } },
+        .{ .mul = .{ .x1, .x1, .x3 } },
+        .{ .div = .{ .x1, .x1, .x2 } },
     });
 }
 
 fn testGen(func: ssa.Function, expected: []const rv64.Instruction) !void {
     const liveness = try ssa.liveness.analyze(std.testing.allocator, func);
     defer liveness.deinit(std.testing.allocator);
-
-    std.debug.print("{}\n", .{func.fmtWithAnnotations(.{liveness})});
 
     const code = try compile(std.testing.allocator, std.testing.allocator, func, liveness);
     defer std.testing.allocator.free(code);
